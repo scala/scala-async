967453eec5c2cab092cbebd5c5ad7207634c7e52
phaller <hallerp@gmail.com>
2012-09-11 09:08:33 +0200

    Remove missing file from build script

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 mkdir -p classes
 scalac -d classes -deprecation -feature src/async/library/scala/async/Async.scala src/async/library/scala/async/AsyncUtils.scala
 mkdir -p test-classes
-scalac -cp classes -d test-classes src/async/test/async-spec/Example.scala
+#scalac -cp classes -d test-classes src/async/test/async-spec/Example.scala
 scalac -cp classes -d test-classes src/async/test/async-spec/MinimalScalaTest.scala
 scalac -cp classes:test-classes -d test-classes src/async/test/async-spec/AsyncSpec.scala
