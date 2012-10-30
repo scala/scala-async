12065e2299c1d2794eaa0bbfee1bea473379b670
phaller <hallerp@gmail.com>
2012-10-31 00:24:31 +0100

    Fix test scripts

--- test-if-else.sh
+++ test-if-else.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p test-classes
-scalac -cp classes -d test-classes src/async/test/async-spec/MinimalScalaTest.scala
-scalac -cp classes:test-classes -d test-classes src/async/test/async-spec/if-else0.scala
-scala -cp test-classes scala.async.Test
+scalac -cp classes -d test-classes test/files/run/if-else0/MinimalScalaTest.scala
+scalac -cp classes:test-classes -d test-classes test/files/run/if-else0/if-else0.scala
+scala -cp test-classes Test
--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p test-classes
-scalac -cp classes -d test-classes src/async/test/async-spec/MinimalScalaTest.scala
-scalac -cp classes:test-classes -d test-classes src/async/test/async-spec/AsyncSpec.scala
-scala -cp test-classes scala.async.Test
+scalac -cp classes -d test-classes test/files/run/block0/MinimalScalaTest.scala
+scalac -cp classes:test-classes -d test-classes test/files/run/block0/AsyncSpec.scala
+scala -cp test-classes Test
