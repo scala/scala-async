ab2d45ef428ba2ae31f262260424ce855248f595
phaller <hallerp@gmail.com>
2012-11-05 08:12:31 +0100

    Remove manual test scripts

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p test-classes
-scalac -cp classes -d test-classes test/files/run/block0/MinimalScalaTest.scala
-scalac -cp classes:test-classes -d test-classes test/files/run/block0/AsyncSpec.scala
-scala -cp test-classes Test
