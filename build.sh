7dbf0a0da4987e8fd5b223437d8f5316ff33616e
phaller <hallerp@gmail.com>
2012-11-07 09:58:44 +0100

    Fix bash build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 scalac -version
 mkdir -p classes
-scalac -P:continuations:enable -d classes -deprecation -feature src/async/library/scala/async/*.scala
+scalac -P:continuations:enable -d classes -deprecation -feature src/main/scala/scala/async/*.scala
