6a2b940ac6b7e511270079e1b6278c844a57f5d1
Jason Zaugg <jzaugg@gmail.com>
2013-11-07 11:29:21 +0100

    Use publishSigned in the release process.

--- release.sh
+++ release.sh
@@ -6,6 +6,11 @@
 #
 #   % cat ~/.sbt/0.13/publish.sbt
 #   credentials += Credentials("Sonatype Nexus Repository Manager", "oss.sonatype.org", "<user>", "<pass>")
+#
+# Also requires the sbt-pgp plugin installed globally to provide the `publishSigned` command.
+#
+#   % cat ~/.sbt/0.13/plugins/gpg.sbt
+#   addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8.1")
 
 function sbt211() {
   sbt 'set scalaVersion := "2.11.0-M6"' 'set scalaBinaryVersion := scalaVersion.value' $@
@@ -17,7 +22,7 @@ die () {
 }
 
 CHECK=";clean;test;publishLocal"
-RELEASE=";clean;test;publish"
+RELEASE=";clean;test;publishSigned"
 VERSION=`gsed -rn 's/version :=.*"(.+).*"/\1/p' build.sbt`
 [[ -n "$(git status --porcelain)" ]] && die "working directory is not clean!"
 
@@ -30,6 +35,6 @@ cat <<EOM
 Released! For non-snapshot releases:
  - tag: git tag -s -a v$VERSION -m "scala-async $VERSION"
  - push tag: git push origin v$VERSION
- - close the staging repository: https://oss.sonatype.org
+ - close and release the staging repository: https://oss.sonatype.org
  - change the version number in build.sbt to a suitable -SNAPSHOT version
 EOM
