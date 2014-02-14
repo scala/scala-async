1b76f83f113143391316e46280cb861cce86ca2e
Eugene Burmako <xeno.by@gmail.com>
2014-02-14 17:59:16 +0100

    injects context into AsyncBase

--- src/main/scala/scala/async/internal/AsyncMacro.scala
+++ src/main/scala/scala/async/internal/AsyncMacro.scala
@@ -4,13 +4,14 @@ import scala.tools.nsc.Global
 import scala.tools.nsc.transform.TypingTransformers
 
 object AsyncMacro {
-  def apply(c: reflect.macros.Context, base: AsyncBase): AsyncMacro = {
+  def apply(c0: reflect.macros.Context, base: AsyncBase): AsyncMacro = {
     import language.reflectiveCalls
-    val powerContext = c.asInstanceOf[c.type { val universe: Global; val callsiteTyper: universe.analyzer.Typer }]
+    val powerContext = c0.asInstanceOf[c0.type { val universe: Global; val callsiteTyper: universe.analyzer.Typer }]
     new AsyncMacro {
+      val c: scala.reflect.macros.Context { val universe: global.type } = c0.asInstanceOf[scala.reflect.macros.Context { val universe: global.type }]
       val global: powerContext.universe.type   = powerContext.universe
       val callSiteTyper: global.analyzer.Typer = powerContext.callsiteTyper
-      val macroApplication: global.Tree        = c.macroApplication.asInstanceOf[global.Tree]
+      val macroApplication: global.Tree        = c0.macroApplication.asInstanceOf[global.Tree]
       // This member is required by `AsyncTransform`:
       val asyncBase: AsyncBase                 = base
       // These members are required by `ExprBuilder`:
@@ -25,6 +26,7 @@ private[async] trait AsyncMacro
   with AnfTransform with TransformUtils with Lifter
   with ExprBuilder with AsyncTransform with AsyncAnalysis with LiveVariables {
 
+  val c: scala.reflect.macros.Context { val universe: global.type }
   val global: Global
   val callSiteTyper: global.analyzer.Typer
   val macroApplication: global.Tree
