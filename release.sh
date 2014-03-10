d76bd53b4a86d748883e3c82e749eaebbc16892c
Jason Zaugg <jzaugg@gmail.com>
2014-03-10 09:35:01 +0100

    only build 2.10.x on this branch

--- release.sh
+++ release.sh
@@ -12,10 +12,6 @@
 #   % cat ~/.sbt/0.13/plugins/gpg.sbt
 #   addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8.1")
 
-function sbt211() {
-  sbt 'set scalaVersion := "2.11.0-M6"' 'set scalaBinaryVersion := scalaVersion.value' $@
-  return $?
-}
 die () {
   echo "$@"
   exit 1
@@ -26,10 +22,8 @@ RELEASE=";clean;test;publishSigned"
 VERSION=`gsed -rn 's/version :=.*"(.+).*"/\1/p' build.sbt`
 [[ -n "$(git status --porcelain)" ]] && die "working directory is not clean!"
 
-sbt211 $CHECK
 sbt $CHECK
 sbt $RELEASE
-sbt211 $RELEASE
 
 cat <<EOM
 Released! For non-snapshot releases:
