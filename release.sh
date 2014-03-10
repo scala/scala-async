1926d792b8fb81df3a0d826ddadfa038e3aa834f
Jason Zaugg <jzaugg@gmail.com>
2014-03-10 09:45:15 +0100

    Update release script, only 2.11 version publised from this branch

--- release.sh
+++ release.sh
@@ -12,10 +12,6 @@
 #   % cat ~/.sbt/0.13/plugins/gpg.sbt
 #   addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8.1")
 
-function sbt211() {
-  sbt 'set scalaVersion := "2.11.0-M6"' 'set scalaBinaryVersion := scalaVersion.value' $@
-  return $?
-}
 die () {
   echo "$@"
   exit 1
@@ -26,15 +22,13 @@ RELEASE=";clean;test;publishSigned"
 VERSION=`gsed -rn 's/version :=.*"(.+).*"/\1/p' build.sbt`
 [[ -n "$(git status --porcelain)" ]] && die "working directory is not clean!"
 
-sbt211 $CHECK
 sbt $CHECK
 sbt $RELEASE
-sbt211 $RELEASE
 
 cat <<EOM
 Released! For non-snapshot releases:
- - tag: git tag -s -a v$VERSION -m "scala-async $VERSION"
- - push tag: git push origin v$VERSION
+ - tag: git tag -s -a v$VERSION_2.11 -m "scala-async $VERSION for Scala 2.11"
+ - push tag: git push origin v$VERSION_2.11
  - close and release the staging repository: https://oss.sonatype.org
  - change the version number in build.sbt to a suitable -SNAPSHOT version
 EOM
