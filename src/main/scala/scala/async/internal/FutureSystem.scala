4752d98e2681e01d7c85030ab9d8f1d67ea99c05
Philippus <philippus@gmail.com>
2019-02-15 20:32:16 +0100

    Relicense under Apache 2, closes #208

--- src/main/scala/scala/async/internal/FutureSystem.scala
+++ src/main/scala/scala/async/internal/FutureSystem.scala
@@ -1,6 +1,15 @@
 /*
- * Copyright (C) 2012-2014 Lightbend Inc. <http://www.lightbend.com>
+ * Scala (https://www.scala-lang.org)
+ *
+ * Copyright EPFL and Lightbend, Inc.
+ *
+ * Licensed under Apache License 2.0
+ * (http://www.apache.org/licenses/LICENSE-2.0).
+ *
+ * See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.
  */
+
 package scala.async.internal
 
 import scala.language.higherKinds
